3bdd51da0a727a73bc52577a620f97f3bd7454ef
fugo8015276 <sf.engineer20231201@gmail.com>
2025-04-28 16:20:42 +0900

    APIの実装(エラー発生中）

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import Search from "./components/Search.jsx";
-import { useState } from "react";
+import { useEffect,useState } from "react";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+
+  const[errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+    } catch (error) {
+      console.error(`Error fetching movies: ${ error }`);
+    }
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main>
       <div className="pattern"/>
@@ -11,10 +38,17 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner"></img>
           <h1>Find <span className="text-gradient">Movies</span>You'll Enjoy
-          Without the Hassle</h1>
-        </header>
+            Without the Hassle</h1>
 
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm } />
+        </header>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text red 500px">{errorMessage}</p>}
+        </section>
+
 
       </div>
     </main>
